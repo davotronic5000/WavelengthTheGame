60102047602f75643f3d940efd2fb3ce9e222836
xiavn <lianne.turnbull@gmail.com>
2020-04-25 21:06:18 +0100

    move colors into main file

--- front-end/src/theme/colors.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    dark: "#493843",
-    primaryDark: "#556869",
-    primary: "#61988E",
-    greyLight: "#CCDAD1",
-    greyMedium: "#9CAEA9",
-    greyDark: "#788585",
-};
--- front-end/src/theme/index.ts
+++ front-end/src/theme/index.ts
@@ -1,7 +1,16 @@
 import styled, { CreateStyled } from "@emotion/styled/macro";
 import colors from "./colors";
 
-export const theme = { colors };
+export const theme = {
+    colors: {
+        dark: "#493843",
+        primaryDark: "#556869",
+        primary: "#61988E",
+        greyLight: "#CCDAD1",
+        greyMedium: "#9CAEA9",
+        greyDark: "#788585",
+    },
+};
 
 export type Theme = typeof theme;
 
