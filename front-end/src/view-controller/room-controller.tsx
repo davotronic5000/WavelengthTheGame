67247ae06cbaf53dec3d4d47a39875b9db7b5285
xiavn <lianne.turnbull@gmail.com>
2020-04-25 20:38:18 +0100

    print team names as an example

--- front-end/src/view-controller/room-controller.tsx
+++ front-end/src/view-controller/room-controller.tsx
@@ -3,7 +3,13 @@ import useRoom from "room-state";
 
 const RoomController: ComponentType<{ roomCode: string }> = ({ roomCode }) => {
     const room = useRoom(roomCode);
-    return <div>Room {roomCode}</div>;
+    return (
+        <div>
+            Room {roomCode}
+            <div>Team 1: {room.team1.players.map((player) => player.name)}</div>
+            <div>Team 2: {room.team2.players.map((player) => player.name)}</div>
+        </div>
+    );
 };
 
 export default RoomController;
