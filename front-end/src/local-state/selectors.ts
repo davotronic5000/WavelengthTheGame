5dd7447ad3081c3781fabde3be226a497230ba08
xiavn <lianne.turnbull@gmail.com>
2020-04-25 22:21:42 +0100

    make selectors correctly return boolenas

--- front-end/src/local-state/selectors.ts
+++ front-end/src/local-state/selectors.ts
@@ -1,9 +1,10 @@
 import { LocalPlayer } from "./types";
 
 export const getName = (localState: LocalPlayer) => localState.name;
-export const hasName = (localState: LocalPlayer) => getName(localState).length;
+export const hasName = (localState: LocalPlayer) =>
+    !!getName(localState).length;
 
 export const getCurrentRoomCode = (localState: LocalPlayer) =>
     localState.currentRoomCode;
 export const hasCurrentRoomCode = (localState: LocalPlayer) =>
-    getCurrentRoomCode(localState).length;
+    !!getCurrentRoomCode(localState).length;
